9a56845629ae550363e913a77e53f05723bd8928
clagenna <clagenna@gmail.com>
2021-05-13 17:12:39 +0200

    tolte un po di risorse

--- lancio_OLD.cmd
+++
@@ -1,9 +0,0 @@
-if "%*" == "" (
-  set /P SORG=Dammi il path completo del dir:
-) else (
-  set SORG=%*
-)
-set LUOGO=%~dp0
-cd /d "%LUOGO%"
-cd
-java --enable-preview -jar target\imaging-jar-with-dependencies.jar -src "%SORG%" -dst work -recurse
