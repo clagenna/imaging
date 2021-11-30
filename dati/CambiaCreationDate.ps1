a87d2d58c248cc8bad9c981e127b5c7afcfed025
Claudio Gennari <clagenna@gmail.com>
2021-11-30 19:37:30 +0100

    corretta la studiaConXYZ

---
+++ dati/CambiaCreationDate.ps1
@@ -0,0 +1,3 @@
+﻿$filnam = "F:\temp\photo\1985\1985-03-21 Ziano2\f19860723_111213.jpg"
+( Get-Item $filnam ).CreationTime = "11/30/1980 12:21:11"
+( Get-Item $filnam ).LastWriteTime  = "07/23/2000 12:20:11"
